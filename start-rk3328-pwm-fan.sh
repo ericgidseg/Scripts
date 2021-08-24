ff474ecee20ffa79af3b9f608e84a133b2dc5eed
ericgidseg <62279163+ericgidseg@users.noreply.github.com>
2021-08-24 11:02:35 +0800

    Update start-rk3328-pwm-fan.sh

--- start-rk3328-pwm-fan.sh
+++ start-rk3328-pwm-fan.sh
@@ -6,10 +6,10 @@ echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable
 while true 
 do         
 temp=$(cat /sys/class/thermal/thermal_zone0/temp)         
-if [ $temp -gt 50000 ]; 
+if [ $temp -gt 53000 ]; 
 then                   #温度可改                 
 echo 100 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
-elif  [ $temp -gt 45000 ];
+elif  [ $temp -gt 48000 ];
 then
 echo 45000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
 else

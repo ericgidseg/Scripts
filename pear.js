0786e105d6154f96c93fa540a71df4d1d2b2fc8a
ericgidseg <62279163+ericgidseg@users.noreply.github.com>
2022-03-26 15:39:51 +0800

    Delete pear.js

--- pear.js
+++
@@ -1,6 +0,0 @@
-[rewrite_local]
-#Pear雪梨解锁会员
-^https:\/\/(cn.youku-ca.com|bkcd\.b-cdn.net|souhu.mett.me|m.pearkin.com|www.baidu.com2.club)\/(api\/movie\/WatchMovieNew|api\/account\/IsVip|api\/account\/IndexDetail) url script-response-body http://ox.xmkczs.com/quantumultX/pear.js
-
-[mitm]
-hostname = bkcd.b-cdn.net,cn.youku-ca.com,souhu.mett.me,m.pearkin.com,www.baidu.com2.club
